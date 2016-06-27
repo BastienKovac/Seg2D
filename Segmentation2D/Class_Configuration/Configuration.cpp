039ed43e6a4989cd02f34d5f10584b3a153d4891
maxime.taisant <maximetaisant@hotmail.fr>
2016-06-27 14:11:40 +0200

    -guguygi

--- Segmentation2D/Class_Configuration/Configuration.cpp
+++ Segmentation2D/Class_Configuration/Configuration.cpp
@@ -65,37 +65,37 @@ Configuration::Configuration(double a_min, double a_max, int size_x, int size_y,
 		Ellips new_ell(a_min,a_max,size_x,size_y);
 		pos=min(nb_rows-1,floor(new_ell.get_cy()/a_max))*nb_col+max(1,ceil(new_ell.get_cx()/a_max));
 
-//		inter=false;
-//		//ind=-1;
-//		#pragma omp parallel for private(t_inter/*, t_ind*/)
-//			//t_ind=ind;
-//			for(int ind = 0; ind < inc; ind++){
-////			while ((inter==false) & (t_ind < inc-1)){
-////				#pragma omp critical
-////				{
-////					ind++;
-////					t_ind = ind;
-////				}
-////				if(t_ind < inc){
-//				if(!inter){
-//					if (is_neighbor(pos,position[ind],nb_rows,nb_col,a_max)){
-//						t_inter=intersect(config[ind],new_ell);
-//					}
-//
-//					if(t_inter){
-//						inter=true;
-//					}
-//				}
-//			}
-
 		inter=false;
-		ind=0;
-		while ((inter==false) & (ind < inc)){
-			if (is_neighbor(pos,position[ind],nb_rows,nb_col,a_max)){
-				inter=intersect(config[ind],new_ell);
+		//ind=-1;
+		#pragma omp parallel for private(t_inter/*, t_ind*/)
+			//t_ind=ind;
+			for(int ind = 0; ind < inc; ind++){
+//			while ((inter==false) & (t_ind < inc-1)){
+//				#pragma omp critical
+//				{
+//					ind++;
+//					t_ind = ind;
+//				}
+//				if(t_ind < inc){
+				if(!inter){
+					if (is_neighbor(pos,position[ind],nb_rows,nb_col,a_max)){
+						t_inter=intersect(config[ind],new_ell);
+					}
+
+					if(t_inter){
+						inter=true;
+					}
+				}
 			}
-			ind++;
-		}
+
+//		inter=false;
+//		ind=0;
+//		while ((inter==false) & (ind < inc)){
+//			if (is_neighbor(pos,position[ind],nb_rows,nb_col,a_max)){
+//				inter=intersect(config[ind],new_ell);
+//			}
+//			ind++;
+//		}
 
 //		if(inter){
 //			cout<<"true"<<endl;
@@ -158,7 +158,7 @@ Configuration::Configuration(double a_min, double a_max, int size_x, int size_y,
 		}
 		if (!(inter)){ // we keep the Ellipse
 			config[inc]=new_ell;
-			data_fit[inc]=new_ell.data_fiting(step,grad_x,grad_y,epsilon,size_x,size_y,d);
+			//data_fit[inc]=new_ell.data_fiting(step,grad_x,grad_y,epsilon,size_x,size_y,d);
 			position[inc]=pos;
 
 			inc++;
@@ -168,6 +168,11 @@ Configuration::Configuration(double a_min, double a_max, int size_x, int size_y,
 			dont_accepted++;
 		} // if (ind==inc)
 	} // while ((inc<50) & (dont_accepted<10))
+
+	#pragma omp parallel for
+	for(int i = 0; i < inc; i++){
+		data_fit[i] = config[i].data_fiting(step,grad_x,grad_y,epsilon,size_x,size_y,d);
+	}
 
 	nb_Ellipses=inc;
 	size=nb_ell;
