9a66738f21a48f9ef210242696b0f3d76aa77826
BastienKovac <bastien.kovac@gmail.com>
2016-06-28 11:48:28 +0200

    -- Added some options in file

---
+++ Segmentation2D/Main_prog/Segmentation_prog.h
@@ -0,0 +1,15 @@
+/*
+ * Segmentation_prog.h
+ *
+ *  Created on: 28 juin 2016
+ *      Author: bastien.kovac
+ */
+
+#ifndef SEGMENTATION_PROG_H_
+#define SEGMENTATION_PROG_H_
+
+
+extern int using_multithread;
+
+
+#endif /* SEGMENTATION_PROG_H_ */
